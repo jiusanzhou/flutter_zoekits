9db5a11244839f51deec1c71021a6b9f09443fdf
Zoe <hi@zoe.im>
2021-06-14 10:35:24 +0800

    :tada: add willpop custom

--- lib/src/extensions/willpop.dart
+++ lib/src/extensions/willpop.dart
@@ -7,19 +7,19 @@ extension PopScope on Widget {
 
   Widget willPop({
     Duration gapDuration = _defaultGapDuration,
-    VoidCallback onPop,
+    WillPopCallback onWillPop,
   }) {
     DateTime _lastTime;
     return WillPopScope(
       child: this,
-      onWillPop: () {
+      onWillPop: () async {
+        // else use the gap duration
         if (_lastTime == null || DateTime.now().difference(_lastTime) > gapDuration) {
-          // TODO: use value from onPop
-          onPop?.call();
           _lastTime = DateTime.now();
-          return Future.value(false);
+          return onWillPop?.call() ?? Future.value(false);
+        } else {
+          return Future.value(true);
         }
-        return Future.value(true);
       },
     );
   }
